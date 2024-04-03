f60608f49fd15c7b91dcf6ef2a2112563b67b3b5
keyur73 <keyurkhambhadiya73@gmail.com>
2024-04-03 13:15:33 +0530

    learning storyboard

--- storyboard learning/ViewController.swift
+++ storyboard learning/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad ()
+        textField.delegate = self
+        textField2.delegate = self
+        self.hideKeyboardTappedAround()
+    }
+    // this function is used for when you press return on keybord it will disapear
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
-}
+    extension UIViewController{
+        func hideKeyboardTappedAround (){
+            let tap:UITapGestureRecognizer = UITapGestureRecognizer (target:self, action:#selector(UIViewController.dismissKeyboard) )
+            tap.cancelsTouchesInView = false
+            view.addGestureRecognizer(tap)
+        }
 
+    @objc func dismissKeyboard(){
+        view.endEditing (true)
+    }
+}
